e0d16c8c928f4b5a6930c7dbcba6543a4bf818f8
TheZink <ilkka.sinkonen@gmail.com>
2025-03-18 14:20:14 +0200

    JavaScript recap 1

--- Javascript1/task5.js
+++ Javascript1/task5.js
@@ -1,7 +1,12 @@
-const input = Number(promt("Give a positive number"))
+const input = Number(prompt("Give a positive number"))
+let sum = 0;
 
 if (input > 0) {
-    for (let i = 0; i < input; i++) {
-        
+    for (let i = 1; i <= input; i++) {
+        sum += i;
     }
+
+document.querySelector(
+    '#target').innerHTML = 
+    `Total sum of natural numbers to number ${input} is ${sum}`
 }
---
+++ Javascript1/task6.js
@@ -0,0 +1,19 @@
+const input = Number(prompt("Give a positive integer"));
+const table = document.getElementById("target");
+
+if (input > 0) {   
+    for (let i = 1; i <= input; i++) {
+        let row = document.createElement("tr");
+
+        for (let j = 1; j <= input; j++) {
+            numbers = i * j;
+            let line = document.createElement("td");
+            line.textContent = numbers;
+            row.appendChild(line)
+        }
+
+        table.appendChild(row);
+    }
+} else {
+    document.querySelector("#p1").innerHTML = "Error: Negative integer!"
+}
